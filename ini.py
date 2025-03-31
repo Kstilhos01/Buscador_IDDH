6b0776f119ace29ed98844ee848d43576b158c59
Kstilhos01 <ctechinformaticascs@gmail.com>
2025-03-30 22:59:34 -0300

    Update ini.py
    update menu item 4

--- ini.py
+++ ini.py
@@ -6,19 +6,23 @@ def exibir_menu():
     print("1 - Buscar Site Senado")
     print("2 - Compara Links Senado")
     print("3 - Extrai dados dos Links")
+    print("4 - Busca Noticas Google")
     print("0 - Sair")
 
 def executar_script(opcao):
     base_path = os.path.join(os.getcwd(), 'scripts')  # Caminho para a pasta 'scripts'
     
     if opcao == "1":
-        script_path = os.path.join(base_path, "busca_senador.py")
+        script_path = os.path.join(base_path, "buscador.py")
         subprocess.run(["python", script_path])
     elif opcao == "2":
         script_path = os.path.join(base_path, "compara.py")
         subprocess.run(["python", script_path])
     elif opcao == "3":
         script_path = os.path.join(base_path, "salvalink.py")
+        subprocess.run(["python", script_path])
+    elif opcao == "4":
+        script_path = os.path.join(base_path, "busca_noticia.py")
         subprocess.run(["python", script_path])
     elif opcao == "0":
         print("Saindo...")
