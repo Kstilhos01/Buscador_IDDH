2a8886cf52851dffaa3d5aca8068a52a194df32c
Kstilhos01 <ctechinformaticascs@gmail.com>
2025-04-02 14:47:40 -0300

    Update ini.py
    inserido opção 5 de instalar as bibliotecas

--- ini.py
+++ ini.py
@@ -6,8 +6,21 @@ def exibir_menu():
     print("1 - Buscar Site Senado")
     print("2 - Compara Links Senado")
     print("3 - Extrai dados dos Links")
-    print("4 - Busca Noticas Google")
+    print("4 - Busca Notícias Google")
+    print("5 - Instalar Dependências do Sistema")
     print("0 - Sair")
+
+def instalar_dependencias():
+    # Lista das bibliotecas a serem instaladas
+    dependencias = ["os", "subprocess", "requests", "pandas", "sqlite3" , "bs4"]
+    
+    for dependencia in dependencias:
+        try:
+            # Executa o comando pip para instalar a biblioteca
+            subprocess.run(["pip", "install", dependencia], check=True)
+            print(f"Dependência '{dependencia}' instalada com sucesso.")
+        except subprocess.CalledProcessError:
+            print(f"Erro ao instalar a dependência '{dependencia}'. Verifique manualmente.")
 
 def executar_script(opcao):
     base_path = os.path.join(os.getcwd(), 'scripts')  # Caminho para a pasta 'scripts'
@@ -24,6 +37,8 @@ def executar_script(opcao):
     elif opcao == "4":
         script_path = os.path.join(base_path, "busca_noticia.py")
         subprocess.run(["python", script_path])
+    elif opcao == "5":
+        instalar_dependencias()
     elif opcao == "0":
         print("Saindo...")
         exit()
